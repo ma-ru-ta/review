6eb49e22dc3cf973cc7ad11a7168f62a6250d4b8
EC2 Default User <ec2-user@ip-172-31-19-133.ap-northeast-1.compute.internal>
2024-01-22 16:38:41 +0000

    devise導入

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
+
+##devise の後ろに :（コロン）で始まる部分が devise の機能名##
+#:database_authenticatable（パスワードの正確性を検証）
+#:registerable（ユーザ登録や編集、削除）
+#:recoverable（パスワードをリセット）
+#:rememberable（ログイン情報を保存）
+#:validatable（email のフォーマットなどのバリデーション）
