5b726ba78b3ee478963c42344ee457aba78206ac
ma-ru-ta <exr.j.mxr@gmail.com>
2024-01-30 08:43:23 +0000

    コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  devise_for :books
   root to: 'homes#top'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
